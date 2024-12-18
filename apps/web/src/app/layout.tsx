1794a0fb5bb6b536c81ddde35060ebf0f2dbdf0d
gabrielmeloc22 <gabrielpresb@gmail.com>
2024-12-18 01:37:12 -0300

    feat(cleanup): cleanup

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,10 @@
+import { ReactNode } from "react"
+
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode
-}) {
+}): ReactNode {
     return (
         <html lang="en">
             <body>{children}</body>
