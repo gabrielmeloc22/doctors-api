f8c240914eb95e6b874e64559ad4783a9d0efec1
gabrielmeloc22 <gabrielpresb@gmail.com>
2024-12-20 21:47:51 -0300

    fix(tsconfig): include config files and fix root dir

--- apps/api/env.d.ts
+++ apps/api/env.d.ts
@@ -3,6 +3,9 @@ declare global {
         interface ProcessEnv {
             NODE_ENV: "development" | "production" | "test";
             PORT?: string;
+            DATABASE_URL: string;
         }
     }
 }
+
+export {};
