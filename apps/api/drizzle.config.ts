476ffb4e7cc9b08b62cf2fbf15215d31efd286e5
gabrielmeloc22 <gabrielpresb@gmail.com>
2024-12-20 21:46:17 -0300

    chore(deps): add drizzle

---
+++ apps/api/drizzle.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+    dialect: "postgresql",
+    schema: "./src/packages/**/*Table.ts",
+    dbCredentials: { url: process.env.DATABASE_URL },
+    out: "./migrations",
+});
